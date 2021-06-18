dca83168947b5892a60b5b1ba72b386d33f4cdcf
maicol <maicol.matteoli@gmail.com>
2021-06-18 15:35:59 +0200

    fine es

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@
 
 //creo e carico il vettore 
 const numbers = [];
-const a = 2;
-const b = 7;
 
 for(let i =0 ; i < 10 ; i++){
     numbers[i] = rand(0, 100);
@@ -14,17 +12,25 @@ for(let i =0 ; i < 10 ; i++){
 
 console.log(numbers);
 
-let newNumbers = numbers.filter((element,index ,a) => {
-    if (index >= a){
-        
-        return true;
-    }
-    else{return false};
-});
+let mandarino = newArray(numbers , 2 , 7);
+console.log(mandarino);
 
-console.log(newNumbers);
+
+//creo il nuovo array "filtrato"
+
 
 // functions
+
+function newArray(vet , min , max){
+    let newNumbers = vet.filter((element,index,array) => { //il terzo parametro array rappresenta l'array di partenza e quindi vet (in questo caso)
+        if (index >= min && index <=max){
+            console.log(array[index]); 
+            return true;
+        }
+        else{return false};
+    });
+    return newNumbers;
+}
 
 function rand(min , max){
     return Math.floor(Math.random() * (max - min + 1) + min);
